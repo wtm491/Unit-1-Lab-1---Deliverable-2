36b350427b92c8de178377dc3d0426e59061cec3
Your Name <you@example.com>
2018-10-25 08:27:10 -0400

    Moved script HTML tag into body. Changed JS file to function implementation

--- script.js
+++ script.js
@@ -1,40 +1,68 @@
 //Javascript logic for deliverable 2!
-// Other implementation was not working upon further testing :(
-// Changed conditionals from typical ALL if statements to else-if statements
-//Initialize result variable only once at top
+//Function implementation - This was fun to code!
+
 
 // Initialize variables to get user input
 var eventType = prompt("Please enter the event type: ");
 var tempFahr = prompt("Please enter the tempature: ");
 var result;
 
-// casual event conditional logic
-if(eventType == "casual" || tempFahr < 54){
- result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear a coat and something comfy!";
-} else if(eventType == "casual" || tempFahr >= 54 || tempFahr <= 70){
- result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear a jacket and something comfy!";
-} else {
- result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear no jacket and something comfy!";
+
+// If casual event - run casualEvent function
+if(eventType == "casual"){
+  casualEvent();
+}
+
+// If semi-formal event - run semiFormalEvent function
+if(eventType == "semi-formal"){
+  semiFormalEvent();
+}
+
+// If formal event - run formalEvent function
+if(eventType == "formal"){
+  formalEvent();
 }
 
 
-//semi-formal event conditional logic
-if(eventType == "semi-formal" || tempFahr < 54){
- result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear a coat and a polo!";
-} else if(eventType == "semi-formal" || tempFahr >= 54 || tempFahr <= 70){
- result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear a jacket and a polo!";
+
+
+// Function for casual event
+function casualEvent(tempFahr){
+if(tempFahr < 54){
+ result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear a coat and SOMETHING COMFY!";
+} else if(tempFahr >= 54 || tempFahr <= 70){
+ result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear a jacket and SOMETHING COMFY!";
 } else {
- result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear no jacket and a polo!";
+ result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear NO jacket and SOMETHING COMFY!";
 }
 
+return result;
+}
 
-//formal event conditional logic
-if(eventType == "formal" || tempFahr < 54){
- result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear a coat and a suit!";
-} else if(eventType == "formal" || tempFahr >= 54 || tempFahr <= 70){
- result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear a jacket and a suit!";
+// Function for semi-formal event
+function semiFormalEvent(tempFahr){
+if(tempFahr < 54){
+ result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear a coat and a POLO!";
+} else if(tempFahr >= 54 || tempFahr <= 70){
+ result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear a jacket and a POLO!";
 } else {
- result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear no jacket and a suit!";
+ result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear NO jacket and a POLO!";
+}
+
+return result;
+}
+
+// Function for formal event
+function formalEvent(tempFahr){
+if(tempFahr < 54){
+ result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear a coat and a SUIT!";
+} else if(tempFahr >= 54 || tempFahr <= 70){
+ result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear a jacket and a SUIT!";
+} else {
+ result = "Since it is " + tempFahr + " degrees out and the event is " + eventType + ", you should wear NO jacket and a SUIT!";
+}
+
+return result;
 }
 
 //Push result string to console
